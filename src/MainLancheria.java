94b38df15eb15b76c43743ad772151feb39684f9
Renato-sch <89554496+Renato-sch@users.noreply.github.com>
2021-10-19 19:20:16 -0300

    Add files via upload

---
+++ src/MainLancheria.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+
+public class MainLancheria {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        Cardapio c1 = new Cardapio();
+        c1.definicaoprecos();
+        c1.definicaoquantidade();
+        
+
+    }    
+}
